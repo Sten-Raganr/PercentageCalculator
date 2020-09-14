294f7f5dca86a853e2f44314c38193a8b7c70905
Opilane TTHK <opilane@tthk.ee>
2020-09-14 14:56:31 +0300

    Create Web Page

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace PercentageCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int maximum;
+            int userscore;
+            double result;
+            Console.WriteLine("What is your maximum score?");
+            maximum = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("What is your score?");
+            userscore = Int32.Parse(Console.ReadLine());
+            result = (userscore * 100 / maximum );
+            Console.WriteLine($"You scored {result}%.");
+            if (result >= 90)
+                Console.WriteLine("You get 5.");
+            else if (result >= 80)
+                Console.WriteLine("You get 4.");
+            else if (result >= 61)
+                Console.WriteLine("You get 3.");
+            else Console.WriteLine("You get 2.");
+
+
+
+
+
+        }
+    }
+}
